d0670068495566856b8a076ed1b0e626353305dc
Mrblackso <164502882+Mrblackso@users.noreply.github.com>
2024-11-21 20:01:44 +0800

    主体

---
+++ pro_class2/src/main/java/dev/handler/CommentHandler.java
@@ -0,0 +1,49 @@
+package dev.handler;
+
+import dev.model.Article;
+import dev.model.Comment;
+import dev.model.User;
+import dev.repository.ArticleRepository;
+import dev.repository.CommentRepository;
+import dev.repository.UserRepository;
+import dev.security.Secured;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/comments")
+public class CommentHandler {
+
+    @Inject
+    private CommentRepository commentRepository;
+
+    @Inject
+    private UserRepository userRepository;
+
+    @Inject
+    private ArticleRepository articleRepository;
+
+    @POST
+    @Path("/")
+    @Secured({"user", "admin"})
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createComment(Comment comment , @Context SecurityContext securityContext ) {
+        User user = userRepository.findByID(Integer.valueOf(securityContext.getUserPrincipal().getName()));
+        comment.setUser(user);
+        Article article = articleRepository.findByID(comment.getArticleId());
+        comment.setArticle(article);
+        comment.setCreatedAt(System.currentTimeMillis() / 1000);
+        commentRepository.create(comment);
+        Map<String, Object> res = new HashMap<>();
+        res.put("code", Response.Status.OK);
+        return Response.status(Response.Status.OK).entity(res).build();
+    }
+
+}
---
+++ pro_class2/src/main/java/dev/handler/IndexHandler.java
@@ -0,0 +1,13 @@
+package dev.handler;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/")  // /api
+public class IndexHandler {
+
+    @GET
+    public String index() {
+        return "Hello World!";
+    }
+}
